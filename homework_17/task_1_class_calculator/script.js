83caa28de9b5e781ad5b180753ee0e2b80fb51f9
nnnssty <asyushakirichenko@gmail.com>
2024-05-20 13:28:32 +0300

    homework 17.1

--- homework_17/task_1_class_calculator/script.js
+++ homework_17/task_1_class_calculator/script.js
@@ -24,23 +24,8 @@ const calc = new Calculator();
 function performOperation(operation) {
     const num1 = parseFloat(document.getElementById('num1').value);
     const num2 = parseFloat(document.getElementById('num2').value);
-    let result;
     try {
-        switch (operation) {
-            case 'add':
-                result = calc.add(num1, num2);
-                break;
-            case 'subtract':
-                result = calc.subtract(num1, num2);
-                break;
-            case 'multiply':
-                result = calc.multiply(num1, num2);
-                break;
-            case 'divide':
-                result = calc.divide(num1, num2);
-                break;
-        }
-        document.getElementById('result').innerText = `Result: ${result}`;
+        document.getElementById('result').innerText = `Result: ${calc[operation](num1, num2)}`;
     } catch (error) {
         document.getElementById('result').innerText = error.message;
     }
