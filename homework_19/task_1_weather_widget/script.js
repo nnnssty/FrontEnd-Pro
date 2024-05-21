7b0d3e7d31e15e8d5188ca5b90b53168bf3e9a21
nnnssty <asyushakirichenko@gmail.com>
2024-05-21 16:06:32 +0300

    homework 19

--- homework_19/task_1_weather_widget/script.js
+++ homework_19/task_1_weather_widget/script.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apiKey = '19c61eafec88724371753c1bc95af0a4';
     const city = 'London';
-    const weatherInfo = document.getElementById('weather-info');
     const cityName = document.getElementById('city-name');
     const temp = document.getElementById('temp');
     const weatherDesc = document.getElementById('weather-desc');
     const refreshButton = document.getElementById('refresh-button');
 
-    function fetchWeather() {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=uk`)
-            .then(response => response.json())
-            .then(data => {
-                cityName.textContent = data.name;
-                temp.textContent = data.main.temp.toFixed(1);
-                weatherDesc.textContent = data.weather[0].description;
-            })
-            .catch(error => console.error('Error fetching weather data:', error));
+    async function fetchWeather() {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=uk`);
+            if (!response.ok) {
+                throw new Error('Error fetching weather data');
+            }
+            const data = await response.json();
+            cityName.textContent = data.name;
+            temp.textContent = data.main.temp.toFixed(1);
+            weatherDesc.textContent = data.weather[0].description;
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
     }
 
     refreshButton.addEventListener('click', fetchWeather);
